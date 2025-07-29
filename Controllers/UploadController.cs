539c3414a9d4a3d05bf1d5484c4083145cac264d
Srinath-g13 <gdivyasrinath@gmail.com>
2025-07-29 13:09:45 +0530

    UploadController Completed

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -8,7 +8,7 @@ namespace BlogManagementApp.Controllers
     {
         private readonly IWebHostEnvironment _Environment;
 
-        public UploadController(IWebHostEnvironment environment,ILogger<UploadController> logger)
+        public UploadController(IWebHostEnvironment environment, ILogger<UploadController> logger)
         {
             _Environment = environment;
         }
@@ -18,11 +18,42 @@ namespace BlogManagementApp.Controllers
         {
             try
             {
-                if(Upload == null || Upload.Length == 0)
+                if (Upload == null || Upload.Length == 0)
                 {
                     return BadRequest(new { error = new { message = "No file uploaded." } });
                 }
-                
+                var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif" };
+                var extensions = Path.GetExtension(Upload.FileName).ToLowerInvariant();
+                if (string.IsNullOrEmpty(extensions) || !allowedExtensions.Contains(extensions))
+                {
+                    return BadRequest(new { error = new { message = "invalid return type" } });
+                }
+                var UniqueFileName = Guid.NewGuid().ToString() + extensions;
+                var UploadsFile = Path.Combine(_Environment.WebRootPath, "uploads");
+                if (!Directory.Exists(UploadsFile))
+                {
+                    Directory.CreateDirectory(UploadsFile);
+                }
+                var FilePath = Path.Combine(UploadsFile, UniqueFileName);
+                using (var stream = new FileStream(FilePath, FileMode.Create))
+                {
+                    await Upload.CopyToAsync(stream);
+                }
+                var response = new
+                {
+                    uploaded = true,
+                    url = Url.Content($"~/Uploads/{UniqueFileName}")
+                };
+                return Ok(response);
+
+            }
+            catch (IOException ioEx)
+            {
+                return StatusCode(500, new { error = new { message = "An error occurred while saving the file. Please try again." } });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { error = new { message = "An unexpected error occurred. Please try again later." } });
             }
         }
     }
